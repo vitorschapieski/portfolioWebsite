d425e12f930a4f99d75c49f10e44dfb544ba89c6
vitorschapieski <fullstackschapieski@gmail.com>
2022-07-03 20:42:48 -0300

    site edit

--- controller/controller.js
+++ controller/controller.js
@@ -2,6 +2,12 @@
 const controller = {
     home:(req,res)=>{
         res.render('index')
+    },
+    gitHub:(req, res) => {
+        res.redirect('https://github.com/vitorschapieski')
+    },
+    linkedin:(req, res) => {
+        res.redirect('https://www.linkedin.com/in/vitor-schapieski-bbb6951b7/')
     }
 };
 
